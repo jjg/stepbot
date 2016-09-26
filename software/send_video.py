ca0807067acf4aa9ec173f13126ae75cce3b26ea
Jason J. Gullickson <jason.gullickson@gmail.com>
2016-09-26 06:43:09 +0000

    more video tweaks

--- software/send_video.py
+++ software/send_video.py
@@ -147,9 +147,9 @@ def handleLinux(deviceNumber, videoPort):
     #commandLine = '/usr/local/bin/ffmpeg -s 320x240 -f video4linux2 -i /dev/video%s -f mpeg1video -b 1k -r 20 http://runmyrobot.com:%s/hello/320/240/' % (deviceAnswer, videoPort)
     #commandLine = '/usr/local/bin/ffmpeg -s 640x480 -f video4linux2 -i /dev/video%s -f mpeg1video -b 150k -r 20 http://%s:%s/hello/640/480/' % (deviceAnswer, server, videoPort)
     # For new JSMpeg
-    #commandLine = '/usr/bin/avconv -f video4linux2 -framerate 25 -video_size 640x480 -i /dev/video%s -f mpegts -codec:v mpeg1video -s 640x480 -b:v 500k -bf 0 http://%s:%s/hello/640/480/' % (deviceAnswer, server, videoPort) # ClawDaddy
+    commandLine = '/usr/bin/avconv -f video4linux2 -framerate 25 -video_size 640x480 -i /dev/video%s -f mpegts -codec:v mpeg1video -s 640x480 -b:v 250k -bf 0 http://%s:%s/hello/640/480/' % (deviceAnswer, server, videoPort) # ClawDaddy
     #commandLine = '/usr/local/bin/ffmpeg -s 1280x720 -f video4linux2 -i /dev/video%s -f mpeg1video -b 1k -r 20 http://runmyrobot.com:%s/hello/1280/720/' % (deviceAnswer, videoPort)
-    commandLine = '/usr/bin/avconv -f video4linux2 -framerate 25 -video_size 640x480 -i /dev/video%s -f mpegts -codec:v mpeg1video -s 640x480 -b:v 500k -bf 0 http://%s:%s/hello/320/240/' % (deviceAnswer, server, videoPort) # Stepbot 
+    #commandLine = '/usr/bin/avconv -f video4linux2 -framerate 25 -video_size 640x480 -i /dev/video%s -f mpegts -codec:v mpeg1video -s 640x480 -b:v 250k -bf 0 http://%s:%s/hello/640/480/' % (deviceAnswer, server, videoPort) # Stepbot 
 
     process = runFfmpeg(commandLine)
 
